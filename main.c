b31314d86df1bcb3dd56a11f73f99a718503332c
coevoe <85091338+KryonicNapkin@users.noreply.github.com>
2025-07-24 07:22:22 +0200

    Added parsing filepaths

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+// TODO: parse the filepath
 
 int main(int argc, char* argv[]) {
-    printf("Hello, world!\n");
-    return 0;
+    if (argc <= 1) {
+        fprintf(stdout, "Usage: scat [FILE]\n");
+        return EXIT_SUCCESS;
+    }
+
+    struct stat st = {0};
+    if (stat(argv[1], &st) == -1) {
+        perror("Error");
+        return EXIT_FAILURE;
+    }
+    printf("%s\n", argv[1]);
+    return EXIT_SUCCESS;
 }
