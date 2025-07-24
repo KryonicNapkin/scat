f1da9342dd214a4ed6e9c1930dee2460a806cb0b
coevoe <85091338+KryonicNapkin@users.noreply.github.com>
2025-07-24 08:52:38 +0200

    Finished scat

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 
 // TODO: parse the filepath
+// TODO: print the file
+
+#define LINE_LENGHT 1024
+
+char* get_entire_file(const char* filepath);
 
 int main(int argc, char* argv[]) {
     if (argc <= 1) {
@@ -10,11 +16,32 @@ int main(int argc, char* argv[]) {
         return EXIT_SUCCESS;
     }
 
-    struct stat st = {0};
-    if (stat(argv[1], &st) == -1) {
+    char* file = get_entire_file(argv[1]);
+    if (file == NULL) {
         perror("Error");
         return EXIT_FAILURE;
     }
-    printf("%s\n", argv[1]);
+    printf("%s", file);
+
+    free(file);
     return EXIT_SUCCESS;
+}
+
+char* get_entire_file(const char* filepath) {
+    FILE* fp = fopen(filepath, "r");
+    if (fp == NULL) return NULL;
+
+    fseek(fp, 0, SEEK_END);
+    size_t fsize = ftell(fp);
+    rewind(fp);
+
+    char* file_string = malloc(fsize);
+    char line[LINE_LENGHT];
+
+    while (fgets(line, LINE_LENGHT, fp) != NULL) {
+        strcat(file_string, line);
+    }
+
+    fclose(fp);
+    return file_string;
 }
